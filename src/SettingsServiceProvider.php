2acabc455788130d9c8ddd19f19c6ff4a53935a9
Randall Wilk <wilk.randall@gmail.com>
2020-08-01 13:26:04 -0500

    Initial commit

--- src/SettingsServiceProvider.php
+++ src/SettingsServiceProvider.php
@@ -10,29 +10,19 @@ class SettingsServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../config/skeleton.php' => config_path('skeleton.php'),
+                __DIR__ . '/../config/settings.php' => config_path('settings.php'),
             ], 'config');
 
-            $this->publishes([
-                __DIR__ . '/../resources/views' => base_path('resources/views/vendor/skeleton'),
-            ], 'views');
-
-            if (! class_exists('CreatePackageTable')) {
+            if (! class_exists('CreateSettingsTable')) {
                 $this->publishes([
-                    __DIR__ . '/../database/migrations/create_skeleton_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_skeleton_table.php'),
+                    __DIR__ . '/../database/migrations/create_settings_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_settings_table.php'),
                 ], 'migrations');
             }
-
-            $this->commands([
-                SkeletonCommand::class,
-            ]);
         }
-
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'skeleton');
     }
 
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/skeleton.php', 'skeleton');
+        $this->mergeConfigFrom(__DIR__ . '/../config/settings.php', 'settings');
     }
 }
