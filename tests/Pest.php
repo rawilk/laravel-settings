1daa25d66687e884698d6b04950e1ec472418a7e
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2026-05-15 17:00:42 -0500

    Simplify test setup by refactoring database driver connection logic and removing redundant migration functions

--- tests/Pest.php
+++ tests/Pest.php
@@ -17,22 +17,5 @@ uses(TestCase::class)->in(__DIR__);
  */
 function setDatabaseDriverConnection(): void
 {
-    $driver = Settings::getDriver();
-    $reflection = new ReflectionClass($driver);
-
-    $property = $reflection->getProperty('connection');
-    $property->setAccessible(true);
-    $property->setValue($driver, DB::connection());
-}
-
-function migrateTestTables(): void
-{
-    $migration = include __DIR__ . '/Support/database/migrations/create_test_tables.php';
-    $migration->up();
-}
-
-function migrateTeams(): void
-{
-    $migration = include __DIR__ . '/../database/migrations/add_settings_team_field.php.stub';
-    $migration->up();
+    (fn () => $this->connection = DB::connection())->call(Settings::driver('database'));
 }
